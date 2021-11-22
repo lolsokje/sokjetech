7ce53cdaa8ee40ceae0da0e121ab3c045c22986a
Alex <alex9521@hotmail.com>
2021-11-22 19:28:48 +0100

    ESLint/PHPCS setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+    ],
+    'parserOptions': {
+        'ecmaVersion': 13,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'vue',
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'vue/multi-word-component-names': 'off',
+    },
+};
