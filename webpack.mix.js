8e96ce1ad329b7b60fae9ff783e2eca47fe5a9e9
Alex <alex9521@hotmail.com>
2021-11-22 18:48:59 +0100

    Basic setup, basic dependency installation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,13 @@
 const mix = require('laravel-mix');
+const path = require('path');
+
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+    },
+  },
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -12,6 +21,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+  .sass('resources/css/app.scss', 'public/css')
+  .vue();
