0b95353620976981d5197ea680d977261c88342e
Alex <alex9521@hotmail.com>
2021-11-22 23:37:19 +0100

    Use .gif avatars when available

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\RedirectResponse as Redirect;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Laravel\Socialite\Facades\Socialite;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -24,7 +25,7 @@ class AuthController extends Controller
             'discord_id' => $discordUser->getId()
         ], [
             'username' => $discordUser->getName(),
-            'avatar' => $discordUser->getAvatar(),
+            'avatar' => $this->getAvatar($discordUser),
             'is_admin' => in_array($discordUser->getId(), config('services.discord.admin_ids')),
         ]);
 
@@ -41,5 +42,23 @@ class AuthController extends Controller
         session()->regenerateToken();
 
         return redirect(route('index'));
+    }
+
+    private function getAvatar(SocialiteUser $user): string
+    {
+        // getAvatar() will always return a string, even if no avatar exists. The user array contains null if no avatar
+        // is set by the user, so that value is checked before assigning the variable.
+        $userAvatar = $user->user['avatar'];
+        $avatar = $userAvatar ? $user->getAvatar() : null;
+
+        if ($avatar && $this->isGifCompatible($userAvatar)) {
+            $avatar = str_replace('.png', '.gif', $avatar);
+        }
+        return $avatar;
+    }
+
+    private function isGifCompatible(string $avatar): bool
+    {
+        return str_starts_with($avatar, 'a_');
     }
 }
