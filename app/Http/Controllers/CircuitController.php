454209e9ed988beec19545f1b522e3c671f4f79f
Alex <alex9521@hotmail.com>
2021-11-25 18:36:52 +0100

    Fixed failing test

--- app/Http/Controllers/CircuitController.php
+++ app/Http/Controllers/CircuitController.php
@@ -11,11 +11,6 @@ use Inertia\Response;
 
 class CircuitController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth']);
-    }
-
     public function index(): Response
     {
         return Inertia::render('Circuits/Index', [
