f5905f87a153f2e18c8e194342a56c1a6074f2f3
Alex <alex9521@hotmail.com>
2021-11-25 16:29:51 +0100

    Updated title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <title>{{ config('app.name') }}</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
 
