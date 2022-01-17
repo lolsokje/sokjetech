4801f109000bffeeca1f702cd4a05e470043eb78
Alex <alex9521@hotmail.com>
2022-01-17 16:47:17 +0100

    Don't show dev controls when no teams/drivers added

--- resources/js/Shared/DriverDevelopment.vue
+++ resources/js/Shared/DriverDevelopment.vue
@@ -1,65 +1,68 @@
 <template>
-	<p v-if="state.error" class="text-danger">{{ state.error }}</p>
-	<div class="row row-cols-lg-auto mb-3">
-		<div class="col-4">
-			<input id="drivers-min" v-model="state.min" class="form-control" type="number">
+	<template v-if="drivers.length">
+		<p v-if="state.error" class="text-danger">{{ state.error }}</p>
+		<div class="row row-cols-lg-auto mb-3">
+			<div class="col-4">
+				<input id="drivers-min" v-model="state.min" class="form-control" type="number">
+			</div>
+
+			<div class="col-4">
+				<input id="drivers-max" v-model="state.max" class="form-control" type="number">
+			</div>
+			<button class="btn btn-primary" @click.prevent="applyDevRanges">Apply</button>
+
+			<div class="col-4">
+				<input id="edit-mode" v-model="state.hideInputs" class="form-check-inline mt-auto me-2"
+					   type="checkbox">
+				<label class="form-check-label" for="edit-mode">Hide inputs</label>
+			</div>
+
+			<div class="ms-auto">
+				<button :disabled="!devCompleted" class="btn btn-success" @click.prevent="runDev">
+					Run dev
+				</button>
+			</div>
 		</div>
 
-		<div class="col-4">
-			<input id="drivers-max" v-model="state.max" class="form-control" type="number">
-		</div>
-		<button class="btn btn-primary" @click.prevent="applyDevRanges">Apply</button>
+		<form @submit.prevent="store">
+			<table id="screenshot-target" class="table table-bordered table-dark">
+				<thead>
+				<tr>
+					<th class="text-center">#</th>
+					<th>Driver</th>
+					<th>Team</th>
+					<th class="text-center">Current</th>
+					<th v-if="inputsHidden" class="text-center">Min</th>
+					<th v-if="inputsHidden" class="text-center">Max</th>
+					<th class="text-center">Dev</th>
+					<th class="text-center">New</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr v-for="driver in form.drivers" :key="driver.id">
+					<td :style="driver.team_style" class="small-centered">{{ driver.number }}</td>
+					<td>{{ driver.full_name }}</td>
+					<td :style="driver.team_style">{{ driver.team_name }}</td>
+					<td class="small-centered">{{ driver.rating }}</td>
+					<td v-if="inputsHidden" class="big-centered px-3">
+						<input v-model="driver.min" class="form-control" type="number">
+					</td>
+					<td v-if="inputsHidden" class="big-centered px-3">
+						<input v-model="driver.max" class="form-control" type="number">
+					</td>
+					<td class="small-centered">{{ driver.dev }}</td>
+					<td class="small-centered">{{ driver.new }}</td>
+				</tr>
+				</tbody>
+			</table>
 
-		<div class="col-4">
-			<input id="edit-mode" v-model="state.hideInputs" class="form-check-inline mt-auto me-2"
-				   type="checkbox">
-			<label class="form-check-label" for="edit-mode">Hide inputs</label>
-		</div>
-
-		<div class="ms-auto">
-			<button :disabled="!devCompleted" class="btn btn-success" @click.prevent="runDev">
-				Run dev
-			</button>
-		</div>
-	</div>
-
-	<form @submit.prevent="store">
-		<table id="screenshot-target" class="table table-bordered table-dark">
-			<thead>
-			<tr>
-				<th class="text-center">#</th>
-				<th>Driver</th>
-				<th>Team</th>
-				<th class="text-center">Current</th>
-				<th v-if="inputsHidden" class="text-center">Min</th>
-				<th v-if="inputsHidden" class="text-center">Max</th>
-				<th class="text-center">Dev</th>
-				<th class="text-center">New</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr v-for="driver in form.drivers" :key="driver.id">
-				<td :style="driver.team_style" class="small-centered">{{ driver.number }}</td>
-				<td>{{ driver.full_name }}</td>
-				<td :style="driver.team_style">{{ driver.team_name }}</td>
-				<td class="small-centered">{{ driver.rating }}</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="driver.min" class="form-control" type="number">
-				</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="driver.max" class="form-control" type="number">
-				</td>
-				<td class="small-centered">{{ driver.dev }}</td>
-				<td class="small-centered">{{ driver.new }}</td>
-			</tr>
-			</tbody>
-		</table>
-
-		<div class="d-flex">
-			<CopyScreenshotButton/>
-			<button :disabled="devCompleted" class="btn btn-primary ms-auto" type="submit">Save dev</button>
-		</div>
-	</form>
+			<div class="d-flex">
+				<CopyScreenshotButton/>
+				<button :disabled="devCompleted" class="btn btn-primary ms-auto" type="submit">Save dev</button>
+			</div>
+		</form>
+	</template>
+	<p v-else>No drivers have been added to any entrants yet</p>
 </template>
 
 <script setup>
--- resources/js/Shared/TeamDevelopment.vue
+++ resources/js/Shared/TeamDevelopment.vue
@@ -1,61 +1,64 @@
 <template>
-	<p v-if="state.error" class="text-danger">{{ state.error }}</p>
-	<div class="row row-cols-lg-auto mb-3">
-		<div class="col-4">
-			<input id="teams-min" v-model="state.min" class="form-control" type="number">
+	<template v-if="teams.length">
+		<p v-if="state.error" class="text-danger">{{ state.error }}</p>
+		<div class="row row-cols-lg-auto mb-3">
+			<div class="col-4">
+				<input id="teams-min" v-model="state.min" class="form-control" type="number">
+			</div>
+
+			<div class="col-4">
+				<input id="teams-max" v-model="state.max" class="form-control" type="number">
+			</div>
+			<button class="btn btn-primary" @click.prevent="applyDevRanges">Apply</button>
+
+			<div class="col-4">
+				<input id="edit-mode" v-model="state.hideInputs" class="form-check-inline mt-auto me-2"
+					   type="checkbox">
+				<label class="form-check-label" for="edit-mode">Hide inputs</label>
+			</div>
+
+			<div class="ms-auto">
+				<button :disabled="!devCompleted" class="btn btn-success" @click.prevent="runDev">
+					Run dev
+				</button>
+			</div>
 		</div>
 
-		<div class="col-4">
-			<input id="teams-max" v-model="state.max" class="form-control" type="number">
-		</div>
-		<button class="btn btn-primary" @click.prevent="applyDevRanges">Apply</button>
+		<form @submit.prevent="store">
+			<table id="screenshot-target" class="table table-bordered table-dark">
+				<thead>
+				<tr>
+					<th>Name</th>
+					<th class="text-center">Current</th>
+					<th v-if="inputsHidden" class="text-center">Min</th>
+					<th v-if="inputsHidden" class="text-center">Max</th>
+					<th class="text-center">Dev</th>
+					<th class="text-center">New</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr v-for="team in form.teams" :key="team.id">
+					<td :style="team.style">{{ team.name }}</td>
+					<td class="small-centered">{{ team.rating }}</td>
+					<td v-if="inputsHidden" class="big-centered px-3">
+						<input v-model="team.min" class="form-control" type="number">
+					</td>
+					<td v-if="inputsHidden" class="big-centered px-3">
+						<input v-model="team.max" class="form-control" type="number">
+					</td>
+					<td class="small-centered">{{ team.dev }}</td>
+					<td class="small-centered">{{ team.new }}</td>
+				</tr>
+				</tbody>
+			</table>
 
-		<div class="col-4">
-			<input id="edit-mode" v-model="state.hideInputs" class="form-check-inline mt-auto me-2"
-				   type="checkbox">
-			<label class="form-check-label" for="edit-mode">Hide inputs</label>
-		</div>
-
-		<div class="ms-auto">
-			<button :disabled="!devCompleted" class="btn btn-success" @click.prevent="runDev">
-				Run dev
-			</button>
-		</div>
-	</div>
-
-	<form @submit.prevent="store">
-		<table id="screenshot-target" class="table table-bordered table-dark">
-			<thead>
-			<tr>
-				<th>Name</th>
-				<th class="text-center">Current</th>
-				<th v-if="inputsHidden" class="text-center">Min</th>
-				<th v-if="inputsHidden" class="text-center">Max</th>
-				<th class="text-center">Dev</th>
-				<th class="text-center">New</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr v-for="team in form.teams" :key="team.id">
-				<td :style="team.style">{{ team.name }}</td>
-				<td class="small-centered">{{ team.rating }}</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="team.min" class="form-control" type="number">
-				</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="team.max" class="form-control" type="number">
-				</td>
-				<td class="small-centered">{{ team.dev }}</td>
-				<td class="small-centered">{{ team.new }}</td>
-			</tr>
-			</tbody>
-		</table>
-
-		<div class="d-flex">
-			<CopyScreenshotButton/>
-			<button :disabled="devCompleted" class="btn btn-primary ms-auto" type="submit">Save dev</button>
-		</div>
-	</form>
+			<div class="d-flex">
+				<CopyScreenshotButton/>
+				<button :disabled="devCompleted" class="btn btn-primary ms-auto" type="submit">Save dev</button>
+			</div>
+		</form>
+	</template>
+	<p v-else>No teams have been added to this season yet</p>
 </template>
 
 <script setup>
