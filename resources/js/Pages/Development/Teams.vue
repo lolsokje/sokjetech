d4d92f738f1b8f8a9f60a7941d84d5afabaca2d6
Alex <alex9521@hotmail.com>
2022-01-17 15:47:02 +0100

    Components for development and reliability

--- resources/js/Pages/Development/Teams.vue
+++ resources/js/Pages/Development/Teams.vue
@@ -1,70 +1,11 @@
 <template>
 	<h2>Team development</h2>
 
-	<p v-if="state.error" class="text-danger">{{ state.error }}</p>
-	<div class="row row-cols-lg-auto mb-3">
-		<div class="col-4">
-			<input id="teams-min" v-model="state.min" class="form-control" type="number">
-		</div>
-
-		<div class="col-4">
-			<input id="teams-max" v-model="state.max" class="form-control" type="number">
-		</div>
-		<button class="btn btn-primary" @click.prevent="applyDevRanges">Apply</button>
-
-		<div class="col-4">
-			<input id="edit-mode" v-model="state.hideInputs" class="form-check-inline mt-auto me-2"
-				   type="checkbox">
-			<label class="form-check-label" for="edit-mode">Hide inputs</label>
-		</div>
-
-		<div class="ms-auto">
-			<button :disabled="!devCompleted" class="btn btn-success" @click.prevent="runDev">
-				Run dev
-			</button>
-		</div>
-	</div>
-
-	<form @submit.prevent="store">
-		<table id="screenshot-target" class="table table-bordered table-dark">
-			<thead>
-			<tr>
-				<th>Name</th>
-				<th class="text-center">Current</th>
-				<th v-if="inputsHidden" class="text-center">Min</th>
-				<th v-if="inputsHidden" class="text-center">Max</th>
-				<th class="text-center">Dev</th>
-				<th class="text-center">New</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr v-for="team in form.teams" :key="team.id">
-				<td :style="team.style">{{ team.name }}</td>
-				<td class="small-centered">{{ team.rating }}</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="team.min" class="form-control" type="number">
-				</td>
-				<td v-if="inputsHidden" class="big-centered px-3">
-					<input v-model="team.max" class="form-control" type="number">
-				</td>
-				<td class="small-centered">{{ team.dev }}</td>
-				<td class="small-centered">{{ team.new }}</td>
-			</tr>
-			</tbody>
-		</table>
-
-		<div class="d-flex">
-			<CopyScreenshotButton/>
-			<button :disabled="devCompleted" class="btn btn-primary ms-auto" type="submit">Save dev</button>
-		</div>
-	</form>
+	<TeamDevelopment :season="season" :teams="teams" :form-route="route('seasons.development.teams.store', [season])"/>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/inertia-vue3';
-import { computed, onMounted, reactive } from 'vue';
-import CopyScreenshotButton from '../../Shared/CopyScreenshotButton';
-import Development from '../../Utilities/Development';
+import TeamDevelopment from '../../Shared/TeamDevelopment';
 
 const props = defineProps({
 	season: {
@@ -75,58 +16,6 @@ const props = defineProps({
 		type: Array,
 		required: true,
 	},
-});
-
-const state = reactive({
-	error: null,
-	devCompleted: false,
-	hideInputs: false,
-	min: 0,
-	max: 0,
-});
-
-const form = useForm({
-	teams: [],
-});
-
-function applyDevRanges () {
-	state.error = null;
-	if (Development.validateDevRange(state)) {
-		Development.applyDevRangesToItems(form.teams, state);
-	} else {
-		state.error = 'The minimum bound must be equal to or lower than the maximum bound.';
-	}
-}
-
-function runDev () {
-	state.error = null;
-	if (Development.performDev(form.teams)) {
-		state.devCompleted = true;
-	} else {
-		state.error = 'One of the teams\' dev ranges are invalid, the minimum bound must be equal to or lower than the maximum bound.';
-	}
-}
-
-function store () {
-	Development.storeDev(form, route('seasons.development.teams.store', [props.season]), state);
-}
-
-const devCompleted = computed(() => !state.devCompleted);
-const inputsHidden = computed(() => !state.hideInputs);
-
-onMounted(() => {
-	props.teams.forEach((team) => {
-		form.teams.push({
-			id: team.id,
-			name: team.team.full_name,
-			style: team.style_string,
-			rating: team.rating,
-			min: 0,
-			max: 0,
-			dev: 0,
-			new: team.rating,
-		});
-	});
 });
 </script>
 
