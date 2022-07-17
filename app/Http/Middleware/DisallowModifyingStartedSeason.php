66626773c563d2d1481886e88de222152ab91845
Alex <alex9521@hotmail.com>
2022-07-17 16:08:28 +0200

    Added missing return type

--- app/Http/Middleware/DisallowModifyingStartedSeason.php
+++ app/Http/Middleware/DisallowModifyingStartedSeason.php
@@ -3,13 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class DisallowModifyingStartedSeason
 {
-    public function handle(Request $request, Closure $next): RedirectResponse|Response
+    public function handle(Request $request, Closure $next): RedirectResponse|Response|JsonResponse
     {
         $season = $request->route('season');
 
