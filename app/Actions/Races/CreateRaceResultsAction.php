8329d319b643d25d0830ff8d23ad8bd5746445b5
Alex <alex9521@hotmail.com>
2022-07-27 12:33:56 +0200

    Store driver, team, engine rating when creating initial race results entry

--- app/Actions/Races/CreateRaceResultsAction.php
+++ app/Actions/Races/CreateRaceResultsAction.php
@@ -20,6 +20,9 @@ class CreateRaceResultsAction
                 'racer_id' => $result->racer_id,
                 'entrant_id' => $result->entrant_id,
                 'season_id' => $result->season_id,
+                'driver_rating' => $result->driver_rating,
+                'team_rating' => $result->team_rating,
+                'engine_rating' => $result->engine_rating,
                 'starting_position' => $result->position,
                 'position' => $result->position,
             ]);
