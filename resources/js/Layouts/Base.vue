7c3e187f85af7813e80ba62a5080b55a2d365a46
Alex <alex9521@hotmail.com>
2022-08-21 20:25:17 +0200

    Display app version in footer

--- resources/js/Layouts/Base.vue
+++ resources/js/Layouts/Base.vue
@@ -1,18 +1,36 @@
 <template>
-    <Nav/>
+    <div class="min-vh-100 d-flex flex-column justify-content-between">
+        <div>
+            <Nav/>
 
-    <div id="content" class="px-5">
-        <Toast/>
+            <div id="content" class="px-5 mb-3">
+                <Toast/>
 
-        <main>
-            <slot/>
-        </main>
+                <main class="flex-shrink-0">
+                    <slot/>
+                </main>
+            </div>
+        </div>
+
+        <footer class="bg-dark text-center">
+            <span id="version" v-if="version"><small>V{{ version }}</small></span>
+        </footer>
     </div>
 </template>
 
 <script setup>
 import Toast from '../Shared/Toast';
-import Nav from '@/Shared/Nav';</script>
+import Nav from '@/Shared/Nav';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const version = ref('');
+
+onMounted(async () => {
+    const response = await axios.get(route('version'));
+    version.value = response.data;
+});
+</script>
 
 <script>
 export default { name: 'Base' };
