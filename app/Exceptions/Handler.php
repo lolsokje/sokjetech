d3b59f80ef6c1a4a08de1e01f43da68fc2a5ed45
Alex <lolsokje@users.noreply.github.com>
2022-08-26 18:46:50 +0200

    Restrict staging access, clean up AuthController (#30)
    * Restrict staging access, clean up AuthController
    
    * Updated github workflow

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
     {
         $response = parent::render($request, $e);
 
-        if (!app()->environment(['local', 'staging']) && in_array($response->status(), [500, 503, 404, 403])) {
+        if (!app()->environment(['local']) && in_array($response->status(), [500, 503, 404, 403])) {
             return Inertia::render('Error', [
                 'status' => $response->status(),
                 'auth' => [
