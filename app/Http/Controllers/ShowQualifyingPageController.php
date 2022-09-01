9425a054e966c7d9da32243b496c3e804ea0c06c
Alex <alex9521@hotmail.com>
2022-09-01 21:04:43 +0200

    Fixed issue where newly added drivers were present in already completed race weekends

--- app/Http/Controllers/ShowQualifyingPageController.php
+++ app/Http/Controllers/ShowQualifyingPageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\QualifyingResultResource;
 use App\Http\Resources\RaceWeekendDriverResource;
 use App\Models\Race;
+use Illuminate\Support\Collection;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -14,22 +15,47 @@ class ShowQualifyingPageController extends Controller
     {
         $this->authorize('view', $race->season->universe);
 
-        $race->load([
-            'season' => [
-                'activeRacers' => [
-                    'driver',
-                    'entrant' => ['engine'],
-                ],
-                'qualifyingFormat',
-            ],
-            'qualifyingResults',
-        ]);
-
-        $drivers = RaceWeekendDriverResource::collection($race->season->activeRacers);
+        $drivers = RaceWeekendDriverResource::collection($this->getRacers($race));
         return Inertia::render('RaceWeekend/Qualifying', [
             'race' => $race,
             'drivers' => $drivers->toArray(request()),
             'qualifyingResults' => QualifyingResultResource::collection($race->qualifyingResults)->toArray(request()),
         ]);
+    }
+
+    private function getRacers(Race $race): Collection
+    {
+        return $race->qualifying_started ? $this->getRacersForRace($race) : $this->getRacersForSeason($race);
+    }
+
+    private function getRacersForRace(Race $race): Collection
+    {
+        $race->load([
+            'season' => ['qualifyingFormat'],
+            'qualifyingResults' => [
+                'racer' => [
+                    'driver',
+                    'entrant' => ['engine'],
+                ],
+            ],
+        ]);
+        return $race->qualifyingResults->map->racer;
+    }
+
+    private function getRacersForSeason(Race $race): Collection
+    {
+        $race->load([
+            'season' => [
+                'qualifyingFormat',
+                'activeRacers' => [
+                    'driver',
+                    'entrant' => [
+                        'engine',
+                    ],
+                ],
+            ],
+        ]);
+
+        return $race->season->activeRacers;
     }
 }
