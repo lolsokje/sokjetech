e8eec31b23b3c9ada841a9044e2976be0ff21b13
Alex <alex9521@hotmail.com>
2023-05-14 19:59:45 +0200

    Re-use ClimateSelect component on circuit create/edit page

--- resources/js/Pages/Circuits/Edit.vue
+++ resources/js/Pages/Circuits/Edit.vue
@@ -11,34 +11,7 @@
 
         <CountrySelect :country="form.country" @countryChanged="setCountry"/>
 
-        <div class="mb-3">
-            <label class="form-label" for="default_climate">Default climate</label>
-            <select id="default_climate"
-                    v-model="form.default_climate_id"
-                    class="form-select"
-                    @change.prevent="setCondition()"
-            >
-                <option v-for="climate in climates" :key="climate.id" :value="climate.id">{{ climate.name }}</option>
-            </select>
-        </div>
-
-        <div class="mb-3" v-if="conditions">
-            <h5>Condition chances</h5>
-            <table class="table table-narrow">
-                <thead>
-                <tr>
-                    <th v-for="condition in conditions" :key="condition.id">{{ condition.condition_name }}</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td class="smallest-centered" v-for="condition in conditions" :key="condition.id">
-                        {{ condition.chance }}%
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+        <ClimateSelect :climates="climates" v-model="form.default_climate_id"/>
 
         <div class="mb-3">
             <input type="checkbox" class="form-check-inline" id="shared" v-model="form.shared">
@@ -56,7 +29,7 @@ import CountrySelect from '@/Shared/CountrySelect.vue';
 import Circuit from '@/Interfaces/Circuit';
 import Breadcrumb from '@/Components/Breadcrumb.vue';
 import Climate from '@/Interfaces/Climate';
-import { onMounted, ref, Ref } from 'vue';
+import ClimateSelect from '@/Components/ClimateSelect.vue';
 
 const props = defineProps<{
     circuit: Circuit,
@@ -75,15 +48,7 @@ const form: InertiaForm<{
     shared: props.circuit.shared,
 });
 
-const conditions: Ref<WeatherConditions> = ref(props.climates[0].conditions);
-
 function setCountry (country: string) {
     form.country = country;
 }
-
-const setCondition = (): void => {
-    conditions.value = props.climates.find((c: Climate) => c.id === form.default_climate_id).conditions;
-};
-
-onMounted(() => setCondition());
 </script>
