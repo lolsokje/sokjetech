c6cc96140c1f10f17150f84ee3526d21732cc2b7
Alex <alex9521@hotmail.com>
2023-06-18 17:37:40 +0200

    Track variation front end

---
+++ resources/js/Interfaces/CircuitVariation.ts
@@ -0,0 +1,7 @@
+export default interface CircuitVariation {
+    id: string,
+    name: string,
+    length: number,
+    base_laptime: number,
+    readable_laptime: string,
+}
--- resources/js/Pages/Circuits/Variations/Create.vue
+++ resources/js/Pages/Circuits/Variations/Create.vue
@@ -1,7 +1,53 @@
 <template>
+    <Breadcrumb :link="route('circuits.edit', circuit)"
+                :linkText="circuit.name"
+                label="Variations"
+                append="Create"
+    />
 
+    <form class="form-narrow" @submit.prevent="form.post(route('circuits.variations.store', circuit))">
+        <Errors :errors="form.errors"/>
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input id="name" type="text" v-model="form.name" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="length" class="form-label">Length (in meters)</label>
+            <input id="length" type="number" v-model="form.length" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="base_laptime" class="form-label">Base laptime (in the format mm:ss.xxx)</label>
+            <input id="base_laptime" type="text" v-model="form.base_laptime" class="form-control" required>
+        </div>
+
+        <button class="btn btn-primary">Save</button>
+    </form>
 </template>
 
 <script lang="ts" setup>
+import Circuit from '@/Interfaces/Circuit';
+import { useForm } from '@inertiajs/vue3';
+import Errors from '@/Shared/Errors.vue';
+import Breadcrumb from '@/Components/Breadcrumb.vue';
 
+interface Props {
+    circuit: Circuit,
+}
+
+interface Form {
+    name: string,
+    length: number,
+    base_laptime: string,
+}
+
+const props = defineProps<Props>();
+
+const form = useForm<Form>({
+    name: '',
+    length: 0,
+    base_laptime: '',
+});
 </script>
