142ad6de07b61352ca1581b3d8efe4ed270fbc87
Alex <alex9521@hotmail.com>
2023-06-19 21:16:14 +0200

    Refactor race create/edit pages to Typescript

--- resources/js/Interfaces/Circuit.ts
+++ resources/js/Interfaces/Circuit.ts
@@ -1,4 +1,5 @@
 import Climate from '@/Interfaces/Climate';
+import CircuitVariation from '@/Interfaces/Circuit/CircuitVariation';
 
 export default interface Circuit {
     id: string,
@@ -8,6 +9,7 @@ export default interface Circuit {
     default_climate_id: string,
     shared: boolean,
     races?: Array<object>,
-    default_climate?: Climate,
+    default_climate: Climate,
     races_count?: number,
+    variations: CircuitVariation[],
 }
